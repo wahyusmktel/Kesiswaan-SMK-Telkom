e8221388a2479b1f9180d3e981d3f40348d43718
Wahyu Rahmat Hidayat <wahyu@smktelkom-lpg.sch.id>
2026-01-04 19:37:19 +0700

    feat: Add initial Laravel application configuration, including AppServiceProvider for HTTPS and global settings, and register Spatie permission middleware aliases.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,6 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
             'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
             'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
         ]);
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
