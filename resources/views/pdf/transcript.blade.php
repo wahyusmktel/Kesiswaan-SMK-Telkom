6ebbf655941b2fb9695964ddcc90c5cf66280bda
Wahyu Rahmat Hidayat <wahyu@smktelkom-lpg.sch.id>
2026-07-06 20:24:34 +0700

    feat(transcript): decrease watermark opacity to 0.2 in PDF transcript

--- resources/views/pdf/transcript.blade.php
+++ resources/views/pdf/transcript.blade.php
@@ -65,7 +65,7 @@
             left: 50%;
             width: 150mm;
             transform: translate(-50%, -50%);
-            opacity: .5;
+            opacity: 0.2;
             z-index: -1;
         }
 
